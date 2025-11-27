67310ead76dfdcb3cc5fbd76ff9756e21327ed2e
22007sujal <2007sujalgmail.com>
2025-11-27 16:35:16 +0530

    added comments

--- shy.js
+++ shy.js
@@ -3,53 +3,59 @@ const { JSDOM } = require("jsdom");
 
 async function Crawl(currentUrl, baseUrl, pages) {
 
-    console.log(`Crawling: ${currentUrl}`);
+    console.log(`Crawling: ${currentUrl}`); //print the current url
 
-    const currentURLObj = new URL(currentUrl);
-    const baseURLObj = new URL(baseUrl);
+    const currentURLObj = new URL(currentUrl); //convert string to url object
+    const baseURLObj = new URL(baseUrl); //convert string to url object
 
   
-    if (currentURLObj.origin !== baseURLObj.origin) {
+    if (currentURLObj.origin !== baseURLObj.origin) { //checks if both base url and current url is from same origin 
+                                                      //because we don,t want to run this function forever so we don,t go outside to fetch link
         return pages;
     }
 
-    const normalized = normalizeURL(currentUrl);
+    const normalized = normalizeURL(currentUrl); //normalize url this function
+                                                 //convert url path and captilized 
+                                                //letter in url to full url
 
 
     if (pages[normalized] > 0) {
-        pages[normalized]++;
         return pages;
-    }
+    } //if we already mapping of website then pages map shoudl greater than 0
+      // so we just return pages
 
-    pages[normalized] = 1;
+    pages[normalized] = 1;//otherwise we set 1 to map for that website we visited
 
     try {
         const resp = await fetch(currentUrl);
 
-        if (resp.status > 399) {
+        if (resp.status > 399) { //check is if website gave correct response
             console.log(`Fetch error on: ${currentUrl}`);
             return pages;
         }
 
         const contentType = resp.headers.get("content-type");
-        if (!contentType || !contentType.includes("text/html")) {
+        if (!contentType || !contentType.includes("text/html")) { // check if we get html
+             //content after fetch
             console.log(`Skipping non-HTML content: ${currentUrl}`);
             return pages;
         }
 
-        const html = await resp.text();
+        const html = await resp.text(); //get all html text 
 
-        const nextUrls = getURLsfromHTML(html, baseURLObj);
+        const nextUrls = getURLsfromHTML(html, baseURLObj);  //get all urls from fetched 
+                                                             //html
 
         for (const url of nextUrls) {
-            await Crawl(url, baseUrl, pages);
+            await Crawl(url, baseUrl, pages);//doing rescursivley calls on every url to scroll all part
+                                             //of same website
         }
 
     } catch (err) {
         console.log(`Error: ${err.message} at ${currentUrl}`);
     }
 
-    return pages;
+    return pages; //return pages 
 }
 
 
